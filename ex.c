42100cb914ed91040eaace719bfa74c23bc7fcbb
anaflv <aflvaraujo@gmail.com>
2018-10-03 15:16:38 +0000

    Add main function

---
+++ ex.c
@@ -0,0 +1,31 @@
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void le(int *v, int n){
+    int i;
+    for(i=0; i<n; i++){
+        scanf("%d ",&v[i]);
+    }
+    
+}
+
+
+int main(){
+    
+    int n;
+    int *v;
+    
+    scanf("%d",&n);
+    printf("%d\n",n);
+    
+    
+    
+}
+
+
+
