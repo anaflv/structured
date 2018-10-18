087e3d3b8e401593aad3904b5a0c5fa0cea2affc
anaflv <aflvaraujo@gmail.com>
2018-10-18 23:43:14 +0000

    Add print multiples file

---
+++ imprime_multiplos.c
@@ -0,0 +1,36 @@
+/*
+
+Faça uma função recursiva que imprima, em
+ordem crescente, os n primeiros múltiplos de
+um número b.
+void imprime_multiplos(int b, int n);
+
+*/
+
+
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int imprime_multiplos(int b, int n){
+    
+    if(n == 1){
+        printf("%d\n",b*n);
+        return b*n;
+    }
+    
+    imprime_multiplos(b,n-1); 
+    printf("%d\n",b*n);
+}
+
+
+
+int main(){
+
+  int b = 6;
+  imprime_multiplos(b,4);
+  return 0;
+    
+}
