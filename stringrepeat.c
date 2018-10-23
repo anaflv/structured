ea1d19e517faa51610181d041567d89b768451cb
anaflv <aflvaraujo@gmail.com>
2018-10-23 10:42:04 +0000

    Update repeat_string

--- stringrepeat.c
+++ stringrepeat.c
@@ -1,9 +1,9 @@
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+/*
 
+Write a function called repeatStr which repeats the given string string exactly n times.
 
+*/
 
 #include <string.h>
 #include <stdlib.h>
@@ -14,19 +14,13 @@ char *repeat_str(int count, const char *src) {
   int i,j;
   int c = 0;
   
-  
-  
   while(src[c] != '\0'){
     c++;
   }
-  printf("%s\n",src);
-  printf("c:%d\n",c);
-  printf("count:%d\n",count);
-
-  char *new_str = malloc(c * sizeof(char));
+  
+  char *new_str = calloc(c * sizeof(char) * count);
   
   i = 0;
-  
   while(i<(count*c)){
     for(j=0;j<c;j++){
       new_str[i] = src[j];
@@ -34,13 +28,12 @@ char *repeat_str(int count, const char *src) {
     }
   }
   
-  
-  
   return new_str;
 }
 
 
 int main(){
     
-    printf("%s",repeat_str(3,"hello "));
+    printf("%s",repeat_str(34,"*a%%Dv6&8(c&lJpV8HCgCicWjdHJ#$8!"));
+    printf("%s",repeat_str(9,"mpGNKC5IO1dIGy0NlLA(oWR1dKtW(jKU"));
 }
