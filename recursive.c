6de7bd410c1362150a4b1278e491193105351ec2
anaflv <aflvaraujo@gmail.com>
2018-11-19 13:57:05 +0000

    Add print vector and factorial

--- recursive.c
+++ recursive.c
@@ -13,8 +13,59 @@ Faça um programa da seguinte forma:
 #include <stdio.h>
 
 
+void inicia_menu(int *vetor, int n);
 
-int main(){
-    
-    
+
+//returns factorial
+int factorial(int n){
+  if (n == 1){
+    return 1;
+  }
+  
+  return n * factorial(n-1);
+}
+
+
+void print_vector(int *vector, int n){
+  
+
+  if (n == 0){
+    printf("%d ",vector[n]);
+    return;
+  }
+  
+  
+  print_vector(vector, n-1);
+  printf("%d ",vector[n]);
+
+}
+
+
+
+
+int main() {
+  inicia_menu(NULL, 0);
+  
+  printf("%d\n",factorial(5));
+  
+  int *vector;
+  vector = malloc(5 * sizeof(int));
+  
+  int i;
+  for (i=0; i<5; i++){
+    vector[i] = i+1;
+  }
+  
+  print_vector(vector, 4);
+  
+  
+  
+  return 0;
+}
+
+
+void inicia_menu(int *vetor, int n) {
+ // Implementacao da funcao
+ 
+  
 }
