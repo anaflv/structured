d02fb5304fa6a3a7f1aa8f4793d3f3a52d07c1ee
anaflv <aflvaraujo@gmail.com>
2018-11-19 13:35:51 +0000

    Add new file

---
+++ recursive.c
@@ -0,0 +1,20 @@
+
+/*
+
+Faça um programa da seguinte forma:
+1. Lê um código de operação;
+2. Executa a operação correspondente;
+3. Volta ao passo 1 para ler outro código de operação.
+
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(){
+    
+    
+}
